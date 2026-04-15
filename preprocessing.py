907095dbb6e224c139ee6e7d22f251c8c8975987
SaraEmad211 <sara.emad2021@gmail.com>
2026-04-15 15:22:49 +0200

    Update preprocessing.py
    feature selection & encoding

--- preprocessing.py
+++ preprocessing.py
@@ -91,7 +91,7 @@ drop_text_cols = [
     'QueryName', 'ResponseName',
     'Website', 'SupportEmail',
     'SupportURL','LegalNotice',
-    'Reviews','SupportedLanguages', 
+    'Reviews','SupportedLanguages',
     'ShortDescrip', 'DetailedDescrip',
     'DRMNotice', 'ExtUserAcctNotice',
     'PriceCurrency',
@@ -125,7 +125,102 @@ X_train[num_cols] = X_train[num_cols].fillna(X_train[num_cols].median())
 X_val[num_cols]   = X_val[num_cols].fillna(X_train[num_cols].median())
 X_test[num_cols]  = X_test[num_cols].fillna(X_train[num_cols].median())
 
+cat_cols = X_train.select_dtypes(include=['object']).columns
+print(cat_cols)
+
+#######################encoding###########################
+X_train = pd.get_dummies(X_train, columns=cat_cols)
+X_val   = pd.get_dummies(X_val, columns=cat_cols)
+X_test  = pd.get_dummies(X_test, columns=cat_cols)
+
+X_val = X_val.reindex(columns=X_train.columns, fill_value=0)
+X_test = X_test.reindex(columns=X_train.columns, fill_value=0)
+##############################################################
 print("\nRemaining NaNs:")
-print(X_train.isnull().sum().sum(), 
-      X_val.isnull().sum().sum(), 
+print(X_train.isnull().sum().sum(),
+      X_val.isnull().sum().sum(),
       X_test.isnull().sum().sum())
+
+print(df.shape)
+
+# print(df.columns)
+
+# print(df.head())
+
+# df.info()
+
+# df.describe(include='all')
+
+# for col, val in df.nunique().sort_values().items():
+#     print(f"{col}: {val}")
+
+#dominant_ratio
+to_drop = []
+
+for col in df.columns:
+    dominant_ratio = df[col].value_counts(normalize=True, dropna=False).max()
+
+    if dominant_ratio >= 0.95:
+        to_drop.append(col)
+
+df.drop(columns=to_drop, inplace=True)
+
+print("Dropped columns:", to_drop)
+
+#VarianceThreshold
+from sklearn.feature_selection import VarianceThreshold
+
+selector = VarianceThreshold(threshold=0.01)
+
+feature_names = X_train.columns
+
+X_train_sel = selector.fit_transform(X_train)
+X_val_sel   = selector.transform(X_val)
+X_test_sel  = selector.transform(X_test)
+
+selected_cols = feature_names[selector.get_support()]
+
+X_train = pd.DataFrame(X_train_sel, columns=selected_cols)
+X_val   = pd.DataFrame(X_val_sel, columns=selected_cols)
+X_test  = pd.DataFrame(X_test_sel, columns=selected_cols)
+
+print("New shape:", X_train.shape)
+print("Selected features:", len(selected_cols))
+
+# Correlation between columns
+corr = X_train.corr().abs()
+upper = corr.where(np.triu(np.ones(corr.shape), k=1).astype(bool))
+
+to_drop = [c for c in upper.columns if any(upper[c] > 0.9)]
+print(to_drop)
+X_train.drop(columns=to_drop, inplace=True)
+X_val.drop(columns=to_drop, inplace=True)
+X_test.drop(columns=to_drop, inplace=True)
+
+#corr with target
+num_cols = X_train.select_dtypes(include=np.number).columns
+
+corr_with_target = X_train[num_cols].corrwith(y_train).abs()
+
+low_corr_features = corr_with_target[corr_with_target < 0.01].index
+
+X_train.drop(columns=low_corr_features, inplace=True)
+X_val.drop(columns=low_corr_features, inplace=True)
+X_test.drop(columns=low_corr_features, inplace=True)
+
+print("Dropped low correlation features:", list(low_corr_features))
+
+#RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectFromModel
+
+selector = SelectFromModel(
+    RandomForestRegressor(n_estimators=100, random_state=42),
+    threshold="median"
+)
+
+selector.fit(X_train, y_train)
+
+X_train_sel = selector.transform(X_train)
+X_val_sel   = selector.transform(X_val)
+X_test_sel  = selector.transform(X_test)
